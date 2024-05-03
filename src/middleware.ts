1771d3302cdafdf47d49567cf6da65eb707e639d
Eugenio Garcia <eugenio5039@gmail.com>
2024-05-02 21:16:41 -0600

    Update - clean middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -8,16 +8,9 @@ export function middleware(request: NextRequest) {
   // Assume a "Cookie:nextjs=fast" header to be present on the incoming request
   // Getting cookies from the request using the `RequestCookies` API
   let cookie = request.cookies.get("authjs.session-token");
-  //   console.log(cookie);
-  //   const allCookies = request.cookies.getAll();
-  //   console.log("ALL");
-  //   console.log(allCookies);
 
   const response = NextResponse.next();
   if (cookie) {
-    // Getting cookies from the request using the `RequestCookies` API
-    // Setting cookies on the response using the `ResponseCookies` API
-
     response.cookies.set({
       name: cookie.name,
       value: cookie.value,
