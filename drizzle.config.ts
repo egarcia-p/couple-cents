c16c5e24326de1a47607e436641788f8e0d8e3dc
Eugenio Garcia <eugenio5039@gmail.com>
2024-05-06 22:32:21 -0600

    Add - drizzle config and current schema

---
+++ drizzle.config.ts
@@ -0,0 +1,10 @@
+import "./drizzle/envConfig";
+import { defineConfig } from "drizzle-kit";
+
+export default defineConfig({
+  schema: "./drizzle/schema.ts",
+  driver: "pg",
+  dbCredentials: {
+    connectionString: process.env.POSTGRES_URL!,
+  },
+});
---
+++ drizzle/schema.ts
@@ -0,0 +1,72 @@
+import { drizzle } from "drizzle-orm/vercel-postgres";
+import { sql } from "@vercel/postgres";
+import {
+  pgTable,
+  serial,
+  text,
+  varchar,
+  timestamp,
+  uniqueIndex,
+  integer,
+  bigint,
+  primaryKey,
+} from "drizzle-orm/pg-core";
+
+export const UsersTable = pgTable(
+  "users",
+  {
+    id: serial("id").primaryKey(),
+    name: varchar("name", { length: 255 }),
+    email: varchar("email", { length: 255 }),
+    emailVerified: timestamp("emailVerified", {
+      withTimezone: true,
+    }),
+
+    image: text("image"),
+  },
+  (users) => {
+    return {
+      uniqueIdx: uniqueIndex("unique_idx").on(users.email),
+    };
+  },
+);
+
+export const AccountsTable = pgTable("accounts", {
+  id: serial("id").primaryKey(),
+  userId: integer("userId").notNull(),
+  type: varchar("type", { length: 255 }).notNull(),
+  provider: varchar("provider", { length: 255 }).notNull(),
+  providerAccountId: varchar("providerAccountId", { length: 255 }).notNull(),
+  refresh_token: text("refresh_token"),
+  access_token: text("access_token"),
+  expires_at: bigint("expires_at", { mode: "bigint" }),
+  id_token: text("id_token"),
+  scope: text("scope"),
+  session_state: text("session_state"),
+  token_type: text("token_type"),
+});
+
+export const SessionsTable = pgTable("sessions", {
+  id: serial("id").primaryKey(),
+  userId: integer("userId").notNull(),
+  expires: timestamp("expires", {
+    withTimezone: true,
+  }).notNull(),
+  sessionToken: varchar("sessionToken", { length: 255 }).notNull(),
+});
+
+export const VerificationTokenTable = pgTable(
+  "verification_token",
+  {
+    identifier: text("identifier").notNull(),
+    expires: timestamp("expires", {
+      withTimezone: true,
+    }).notNull(),
+    token: text("token").notNull(),
+  },
+  (table) => {
+    return {
+      pk: primaryKey({ columns: [table.identifier, table.token] }),
+    };
+  },
+);
