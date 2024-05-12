3bf60763bdf513947142ebb1047cf73dc62e6d84
Eugenio Garcia <eugenio5039@gmail.com>
2024-05-11 18:00:14 -0600

    Update - responsiveness and red

---
+++ src/app/ui/transactions/table-mobile.tsx
@@ -0,0 +1,37 @@
+import { fetchTransactionsByUserId } from "@/app/lib/data";
+import { auth } from "../../../auth";
+import { DeleteTransaction, UpdateTransaction } from "./buttons";
+
+export default async function DashboardTableMobile({
+  userId,
+}: {
+  userId: string;
+}) {
+  const session = await auth();
+
+  if (!session) return null;
+
+  if (!session.user) return null;
+
+  const transactions = await fetchTransactionsByUserId(userId);
+
+  return (
+    <div className="mt-2 flex-row ">
+      <div className="min-w-full rounded-lg bg-gray-50 p-2 md:pt-0">
+        {transactions?.map((transaction) => (
+          <div
+            key={transaction.id}
+            className="flex flex-row justify-between bg-white m-1 rounded-lg"
+          >
+            <div className="py-3 pl-6 pr-3">
+              <span>{transaction.note}</span>
+            </div>
+            <div className="py-3 pl-6 pr-3 font-bold ">
+              <span>${transaction.amount}</span>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
