5b47c3bc7d179142fa22260f3bbb7a7ef20e6cf2
Eugenio Garcia <eugenio5039@gmail.com>
2024-05-25 18:03:35 -0600

    Update - colors and logo

--- src/app/ui/dashboard/sidenav.tsx
+++ src/app/ui/dashboard/sidenav.tsx
@@ -2,15 +2,19 @@ import Link from "next/link";
 import NavLinks from "@/app/ui/dashboard/nav-links";
 import { signOut } from "@/auth";
 import { ArrowLeftEndOnRectangleIcon } from "@heroicons/react/24/outline";
+import logo from "../../../../public/logo.svg";
+import Image from "next/image";
 
 export default function SideNav() {
   return (
     <div className="flex h-full flex-col px-3 py-4 md:px-2">
       <Link
-        className="mb-2 flex h-20 items-end justify-start rounded-md bg-red-600 p-4 md:h-40"
+        className="mb-2 flex h-20 items-end justify-start rounded-md bg-primary-600 p-4 md:h-40"
         href="/"
       >
-        <div className="w-32 text-white md:w-40">Logo TODO</div>
+        <div className="w-32 m-auto text-white md:w-40">
+          <Image src={logo} alt="Logo" />
+        </div>
       </Link>
       <div className="flex grow flex-row justify-between space-x-2 md:flex-col md:space-x-0 md:space-y-2">
         <NavLinks />
@@ -21,7 +25,7 @@ export default function SideNav() {
             await signOut();
           }}
         >
-          <button className="flex h-[48px] w-full grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-red-100 hover:text-black-600 md:flex-none md:justify-start md:p-2 md:px-3">
+          <button className="flex h-[48px] w-full grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-primary-100 hover:text-black-600 md:flex-none md:justify-start md:p-2 md:px-3">
             <ArrowLeftEndOnRectangleIcon className="w-6" />
             <div className="hidden md:block">Sign Out</div>
           </button>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,5 @@
 import type { Config } from "tailwindcss";
+const colors = require("tailwindcss/colors");
 
 const config: Config = {
   content: [
@@ -13,6 +14,23 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+    },
+    colors: {
+      transaparent: "transparent",
+      current: "currentColor",
+      primary: {
+        100: "#C1FFCA",
+        300: "#378743",
+        600: "#225329",
+      },
+      secondary: "#D6AF57",
+      black: colors.black,
+      white: colors.white,
+      gray: colors.gray,
+      emerald: colors.emerald,
+      indigo: colors.indigo,
+      yellow: colors.yellow,
+      red: colors.red,
     },
   },
   plugins: [],
