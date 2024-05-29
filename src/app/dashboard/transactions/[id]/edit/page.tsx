8b88064153e8fe7c6c2d51d03633b2f410d0d7f7
Eugenio Garcia <eugenio5039@gmail.com>
2024-05-28 21:56:33 -0600

    Add - income categories

--- src/app/dashboard/transactions/[id]/edit/page.tsx
+++ src/app/dashboard/transactions/[id]/edit/page.tsx
@@ -2,7 +2,8 @@ import { notFound } from "next/navigation";
 import { Metadata } from "next";
 import Form from "@/app/ui/transactions/edit-form";
 import { fetchTransactionById } from "@/app/lib/data";
-import categories from "@/app/lib/data/categories.json";
+import _categories from "../../../../lib/data/categories.json";
+import _categoriesForIncome from "@/app/lib/data/categoriesForIncome.json";
 
 export const metadata: Metadata = {
   title: "Edit",
@@ -15,9 +16,19 @@ export default async function Page({ params }: { params: { id: string } }) {
     notFound();
   }
 
+  const isExpense = transaction.isExpense;
+  let formCategories = {};
+  if (isExpense) {
+    formCategories = _categories;
+  } else {
+    formCategories = _categoriesForIncome;
+  }
+
   return (
     <main>
-      <Form transaction={transaction} categories={categories} />
+      {isExpense && <h1 className="  text-lg">Edit Expense</h1>}
+      {!isExpense && <h1 className="  text-lg">Edit Income</h1>}
+      <Form transaction={transaction} categories={formCategories} />
     </main>
   );
 }
