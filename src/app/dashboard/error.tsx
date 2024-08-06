ce03a353a7acef00d93c97c197df837c741d83a8
egarcia-p <eugenio5039@gmail.com>
2024-08-05 21:55:13 -0500

    Add - error handling for other errors

---
+++ src/app/dashboard/error.tsx
@@ -0,0 +1,31 @@
+"use client"; // Error components must be Client Components
+
+import { useEffect } from "react";
+
+export default function Error({
+  error,
+  reset,
+}: {
+  error: Error & { digest?: string };
+  reset: () => void;
+}) {
+  useEffect(() => {
+    // Log the error to an error reporting service
+    console.error(error);
+  }, [error]);
+
+  return (
+    <div className="flex flex-col mx-auto p-8 gap-2">
+      <h2 className="text-5xl">Something went wrong!</h2>
+      <button
+        className="text-left underline"
+        onClick={
+          // Attempt to recover by trying to re-render the segment
+          () => reset()
+        }
+      >
+        Try again
+      </button>
+    </div>
+  );
+}
