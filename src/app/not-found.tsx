7f51be63fc69e0df0e14c772fcaf3c28100a2bd7
egarcia-p <eugenio5039@gmail.com>
2024-08-05 21:48:58 -0500

    Add - style to not found

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -2,10 +2,12 @@ import Link from "next/link";
 
 export default function NotFound() {
   return (
-    <div>
-      <h2>Not Found</h2>
-      <p>Could not find requested resource</p>
-      <Link href="/">Return Home</Link>
+    <div className="flex flex-col mx-auto p-8">
+      <h2 className="text-5xl">Not Found</h2>
+      <p>Could not find requested page</p>
+      <Link className="underline" href="/">
+        Return Home
+      </Link>
     </div>
   );
 }
