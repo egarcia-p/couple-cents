32e2f62c4f3b5742c6c533df07365db00f6a23b1
egarcia-p <eugenio5039@gmail.com>
2024-08-14 22:27:59 -0500

    Change - data orm to lib

--- src/app/api/transactions/[userId]/route.ts
+++ src/app/api/transactions/[userId]/route.ts
@@ -8,6 +8,7 @@ import _categoriesIncome from "@/app/lib/data/categoriesForIncome.json";
 import { auth } from "@/auth";
 import { error } from "console";
 import { NextApiRequest, NextApiResponse } from "next";
+import { fetchAllTransactions } from "@/app/lib/data";
 
 interface ICategories {
   [key: string]: string;
@@ -38,41 +39,5 @@ export async function GET(
     const data = await fetchAllTransactions(params.userId);
 
     return Response.json({ message: "Success", data }, { status: 200 });
-  }
-}
-
-export async function fetchAllTransactions(userId: string) {
-  try {
-    const data = await db
-      .select({
-        id: transactions.id,
-        userId: transactions.userId,
-        transactionDate: transactions.transactionDate,
-        category: sql<string>`category`.mapWith({
-          mapFromDriverValue: (value: string) => {
-            const mappedValue = categoriesMap[value];
-            return mappedValue;
-          },
-        }),
-        establishment: transactions.establishment,
-        isExpense: transactions.isExpense,
-        isEssential: transactions.isEssential,
-        note: transactions.note,
-        amount: sql<string>`amount`.mapWith({
-          mapFromDriverValue: (value: any) => {
-            //let mappedValue = value / 100;
-            const mappedValue = formatCurrency(Number(value) ?? "0");
-            return mappedValue;
-          },
-        }),
-      })
-      .from(transactions)
-      .where(eq(transactions.userId, parseInt(userId)))
-      .orderBy(desc(transactions.transactionDate));
-
-    return data;
-  } catch (error) {
-    console.error("Database Error:", error);
-    throw new Error("Failed to fetch transactions.");
   }
 }
