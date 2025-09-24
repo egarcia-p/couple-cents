49c011e15ed3ab9aabc5ce9976b26eeff8409e8c
Eugenio Garcia <eugenio5039@gmail.com>
2025-09-23 20:48:28 -0600

    Update - middleware checks advanced

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,19 +1,33 @@
 import { NextRequest, NextResponse } from "next/server";
 import { getSessionCookie } from "better-auth/cookies";
 
+const protectedRoutes = ["/dashboard"];
+const publicRoutes = ["/login", "/"];
+
 export async function middleware(request: NextRequest) {
+  const path = request.nextUrl.pathname;
+  const isProtectedRoute = protectedRoutes.includes(path);
+  const isPublicRoute = publicRoutes.includes(path);
+
   const sessionCookie = getSessionCookie(request);
 
-  // THIS IS NOT SECURE!
-  // This is the recommended approach to optimistically redirect users
-  // We recommend handling auth checks in each page/route
-  if (!sessionCookie) {
+  if (isProtectedRoute && !sessionCookie) {
     return NextResponse.redirect(new URL("/", request.url));
+  }
+
+  //This is not enough, we need to check if the user is authenticated
+  if (
+    isPublicRoute &&
+    sessionCookie &&
+    !request.nextUrl.pathname.startsWith("/dashboard")
+  ) {
+    return NextResponse.redirect(new URL("/dashboard", request.url));
   }
 
   return NextResponse.next();
 }
 
+// Routes Middleware should not run on
 export const config = {
-  matcher: ["/dashboard"], // Specify the routes the middleware applies to
+  matcher: ["/((?!api|_next/static|_next/image|.*\\.png$).*)"],
 };
