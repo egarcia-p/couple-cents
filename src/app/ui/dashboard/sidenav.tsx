bffdcc5f7d1dc178fe2dec30a27e742127cf55d2
Eugenio Garcia <eugenio5039@gmail.com>
2025-09-23 20:30:32 -0600

    Add - signout with betterauth

--- src/app/ui/dashboard/sidenav.tsx
+++ src/app/ui/dashboard/sidenav.tsx
@@ -1,11 +1,24 @@
+"use client";
+
 import Link from "next/link";
 import NavLinks from "@/app/ui/dashboard/nav-links";
-import { signOut } from "@/auth";
 import { ArrowLeftEndOnRectangleIcon } from "@heroicons/react/24/outline";
 import logo from "../../../../public/logo.svg";
 import Image from "next/image";
+import { useRouter } from "next/navigation";
+import { authClient } from "@/app/lib/auth-client";
 
 export default function SideNav() {
+  const router = useRouter();
+
+  const handleSignOut = async () => {
+    try {
+      await authClient.signOut();
+      router.push("/");
+    } catch (error) {
+      console.error("Sign out error:", error);
+    }
+  };
   return (
     <div className="flex h-full flex-col px-3 py-2 md:px-2 md:py-4">
       <Link className="" href="/">
@@ -24,17 +37,14 @@ export default function SideNav() {
       <div className="flex grow flex-row justify-between space-x-2 md:flex-col md:space-x-0 md:space-y-2">
         <NavLinks />
         <div className="hidden h-auto w-full grow rounded-md bg-gray-50 md:block"></div>
-        <form
-          action={async () => {
-            "use server";
-            await signOut();
-          }}
+
+        <button
+          onClick={handleSignOut}
+          className="flex h-[48px] w-full grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-primary-100 hover:text-black-600 md:flex-none md:justify-start md:p-2 md:px-3"
         >
-          <button className="flex h-[48px] w-full grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-primary-100 hover:text-black-600 md:flex-none md:justify-start md:p-2 md:px-3">
-            <ArrowLeftEndOnRectangleIcon className="w-6" />
-            <div className="hidden md:block">Sign Out</div>
-          </button>
-        </form>
+          <ArrowLeftEndOnRectangleIcon className="w-6" />
+          <div className="hidden md:block">Sign Out</div>
+        </button>
       </div>
     </div>
   );
