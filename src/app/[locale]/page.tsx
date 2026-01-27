f88aea04645786ed7223f0f1a0c792d67260a8d2
Eugenio Garcia <eugenio5039@gmail.com>
2026-01-26 20:41:18 -0600

    Add - supported favicons

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -28,7 +28,96 @@ export default function Home() {
     <>
       <Head>
         <title>{t("title")}</title>
-        <link rel="icon" href="/favicon.ico" />
+        <link
+          key="apple-touch-icon-57x57"
+          rel="apple-touch-icon"
+          sizes="57x57"
+          href="/apple-icon-57x57.png"
+        />
+        <link
+          key="apple-touch-icon-60x60"
+          rel="apple-touch-icon"
+          sizes="60x60"
+          href="/apple-icon-60x60.png"
+        />
+        <link
+          key="apple-touch-icon-72x72"
+          rel="apple-touch-icon"
+          sizes="72x72"
+          href="/apple-icon-72x72.png"
+        />
+        <link
+          key="apple-touch-icon-76x76"
+          rel="apple-touch-icon"
+          sizes="76x76"
+          href="/apple-icon-76x76.png"
+        />
+        <link
+          key="apple-touch-icon-114x114"
+          rel="apple-touch-icon"
+          sizes="114x114"
+          href="/apple-icon-114x114.png"
+        />
+        <link
+          key="apple-touch-icon-120x120"
+          rel="apple-touch-icon"
+          sizes="120x120"
+          href="/apple-icon-120x120.png"
+        />
+        <link
+          key="apple-touch-icon-144x144"
+          rel="apple-touch-icon"
+          sizes="144x144"
+          href="/apple-icon-144x144.png"
+        />
+        <link
+          key="apple-touch-icon-152x152"
+          rel="apple-touch-icon"
+          sizes="152x152"
+          href="/apple-icon-152x152.png"
+        />
+        <link
+          key="apple-touch-icon-180x180"
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-icon-180x180.png"
+        />
+        <link
+          key="icon-192x192"
+          rel="icon"
+          type="image/png"
+          sizes="192x192"
+          href="/android-icon-192x192.png"
+        />
+        <link
+          key="icon-32x32"
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          key="icon-96x96"
+          rel="icon"
+          type="image/png"
+          sizes="96x96"
+          href="/favicon-96x96.png"
+        />
+        <link
+          key="icon-16x16"
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link
+          key="manifest-json-link-tag-key-value-pair-href-value-manifest-json-value"
+          rel="manifest"
+          href="/manifest.json"
+        />
+        <meta name="msapplication-TileColor" content="#ffffff" />
+        <meta name="msapplication-TileImage" content="/ms-icon-144x144.png" />
+        <meta name="theme-color" content="#ffffff"></meta>
       </Head>
       <div id="top" className="sm:h16 lg:h-32 flex"></div>
 
