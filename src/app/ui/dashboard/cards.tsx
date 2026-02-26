534a72de7b0d50cc0970c8f9bb5b7cd6630e245a
Eugenio Garcia <eugenio5039@gmail.com>
2026-02-25 21:16:35 -0600

    Add - line clamp to cards

--- src/app/ui/dashboard/cards.tsx
+++ src/app/ui/dashboard/cards.tsx
@@ -26,7 +26,9 @@ export function Card({
     <div className="rounded-xl bg-gray-50 p-2 shadow-sm">
       <div className="flex p-4">
         {Icon ? <Icon className="h-5 w-5 text-gray-700" /> : null}
-        <h3 className="ml-2 text-sm font-medium">{title}</h3>
+        <h3 className="ml-2 text-sm font-medium line-clamp-1" title={title}>
+          {title}
+        </h3>
       </div>
       <p
         className={`
