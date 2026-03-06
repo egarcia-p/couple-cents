761a9fcb7b83aec380f321571f756e843ef7e934
Eugenio Garcia <eugenio5039@gmail.com>
2026-03-05 20:55:28 -0600

    add - redirect router config

--- next.config.mjs
+++ next.config.mjs
@@ -3,5 +3,16 @@ import createNextIntlPlugin from "next-intl/plugin";
 const withNextIntl = createNextIntlPlugin();
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  rewrites: async () => {
+    return {
+      beforeFiles: [
+        {
+          source: "/:path((?!maintenance\\.html|_next|static).*)",
+          destination: "/maintenance.html",
+        },
+      ],
+    };
+  },
+};
 export default withNextIntl(nextConfig);
